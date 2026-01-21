dfb8d1056d7dd5bebc0fcb147db592b9e68d4106
saifchaudhry <schaudhry722@gmail.com>
2026-01-21 18:53:18 +0500

    Add missing location and shop route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Health check route for Vercel
+  # Health check
   get '/health', to: proc { [200, { 'Content-Type' => 'text/plain' }, ['OK']] }
+
+  # Locations and nested shops
+  resources :locations, only: [:index], param: :slug
+  get '/:slug', to: 'locations#show', as: :location
+
+  scope '/:location_slug', as: :location do
+    resources :shops, only: [:index, :show]
+  end
 
   root "pages#home"
 end
